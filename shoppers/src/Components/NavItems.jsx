0f1396720d31f5b83dc6ccd28f0cab331bbadec4
Nikhil <inikhilmalik@gmail.com>
2023-02-26 15:22:41 +0530

    finished

---
+++ shoppers/src/Components/NavItems.jsx
@@ -0,0 +1,33 @@
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuItemOption,
+    MenuGroup,
+    MenuOptionGroup,
+    MenuDivider,
+  } from '@chakra-ui/react'
+
+<Menu>
+  <MenuButton
+    as={IconButton}
+    aria-label='Options'
+    icon={<HamburgerIcon />}
+    variant='outline'
+  />
+  <MenuList>
+    <MenuItem icon={<AddIcon />} command='⌘T'>
+      New Tab
+    </MenuItem>
+    <MenuItem icon={<ExternalLinkIcon />} command='⌘N'>
+      New Window
+    </MenuItem>
+    <MenuItem icon={<RepeatIcon />} command='⌘⇧N'>
+      Open Closed Tab
+    </MenuItem>
+    <MenuItem icon={<EditIcon />} command='⌘O'>
+      Open File...
+    </MenuItem>
+  </MenuList>
+</Menu>
